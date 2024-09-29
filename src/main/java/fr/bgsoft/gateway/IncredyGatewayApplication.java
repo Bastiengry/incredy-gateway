3d2a1a951302fa3cc24eaf13546be017dc9a950e
bgry <bgryspee@gmail.com>
2024-09-29 11:18:55 +0200

    first commit

---
+++ src/main/java/fr/bgsoft/gateway/IncredyGatewayApplication.java
@@ -0,0 +1,13 @@
+package fr.bgsoft.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncredyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncredyGatewayApplication.class, args);
+	}
+
+}
